a94fa52e2797b038859b87fa56a606ec9d2387a4
Alex <a.isaakiani@gmail.com>
2023-01-17 10:31:00 +0400

    Add modified header component update 1

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -1,12 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
+
+const underlineDecoration = css`
+  border-bottom: 3px solid var(--linkColor);
+`;
 
 export const NavLink = styled(Link)<{ $isCurrentRoute: boolean }>`
   --linkColor: ${({ $isCurrentRoute }) => ($isCurrentRoute ? "blue" : "black")};
 
   text-decoration: none;
   color: var(--linkColor);
-  border-bottom: 3px solid var(--linkColor);
+  ${({ $isCurrentRoute }) => $isCurrentRoute && underlineDecoration};
   padding-bottom: 4px;
 `;
 
