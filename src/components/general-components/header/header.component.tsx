3733156b6085fdf5138868eee458156c31e2818e
Alex <a.isaakiani@gmail.com>
2023-01-24 19:37:03 +0400

    Add responsive navigation menu

--- src/components/general-components/header/header.component.tsx
+++ src/components/general-components/header/header.component.tsx
@@ -1,31 +1,83 @@
-import { useLocation } from "react-router-dom";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
-import { HomeLink, NavLink } from "./header.styles";
+import {
+  CharactersLink,
+  EpisodesLink,
+  HomeLink,
+  LocationsLink,
+  NavLink
+} from "./header.styles";
 
 const Header = () => {
-  const { pathname } = useLocation();
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
     <AppBar position="static" color="default" sx={{ boxShadow: 0 }}>
-      <Toolbar sx={{ flexWrap: "wrap" }}>
+      <Toolbar sx={{ flexWrap: "wrap", display: { xs: "none", md: "flex" } }}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <HomeLink to="/">Rick and Morty Reference Book</HomeLink>
         </Typography>
-        <Typography variant="button">
-          <NavLink to="/characters" $isCurrentRoute={pathname === "/characters"}>
-            Characters
-          </NavLink>
-        </Typography>
-        <Typography variant="button" ml={2}>
-          <NavLink to="/episodes" $isCurrentRoute={pathname === "/episodes"}>
-            Episodes
-          </NavLink>
-        </Typography>
-        <Typography variant="button" ml={2}>
-          <NavLink to="/locations" $isCurrentRoute={pathname === "/locations"}>
-            Locations
-          </NavLink>
-        </Typography>
+        <CharactersLink />
+        <EpisodesLink sx={{ marginLeft: 2 }} />
+        <LocationsLink sx={{ marginLeft: 2 }} />
+      </Toolbar>
+
+      <Toolbar sx={{ flexWrap: "wrap", display: { xs: "flex", md: "none" } }}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          onClick={handleOpenNavMenu}
+          color="inherit"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left"
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left"
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: "block", md: "none" }
+          }}
+        >
+          <MenuItem>
+            <CharactersLink />
+          </MenuItem>
+          <MenuItem>
+            <EpisodesLink />
+          </MenuItem>
+          <MenuItem>
+            <LocationsLink />
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
--- src/components/general-components/header/header.styles.tsx
+++ src/components/general-components/header/header.styles.tsx
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { Typography } from "@mui/material";
 
 const underlineDecoration = css`
   border-bottom: 3px solid var(--linkColor);
@@ -18,3 +19,39 @@ export const HomeLink = styled(Link)`
   text-decoration: none;
   color: black;
 `;
+
+export const CharactersLink = ({ sx = {} }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Typography variant="button" sx={sx}>
+      <NavLink to="/characters" $isCurrentRoute={pathname === "/characters"}>
+        Characters
+      </NavLink>
+    </Typography>
+  );
+};
+
+export const EpisodesLink = ({ sx = {} }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Typography variant="button" sx={sx}>
+      <NavLink to="/episodes" $isCurrentRoute={pathname === "/episodes"}>
+        Episodes
+      </NavLink>
+    </Typography>
+  );
+};
+
+export const LocationsLink = ({ sx = {} }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Typography variant="button" sx={sx}>
+      <NavLink to="/locations" $isCurrentRoute={pathname === "/locations"}>
+        Locations
+      </NavLink>
+    </Typography>
+  );
+};
